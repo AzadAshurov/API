b86a6b9c2b7f01c86ee4347cecf0ca1f9d4f2d66
Wertdrof <wertdrof@gmail.com>
2024-12-23 12:56:47 +0400

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using API.DAL;
+using API.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            // return Ok(categories);
+            return StatusCode(StatusCodes.Status200OK, categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id < 1) return BadRequest();
+
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (category == null) return NotFound();
+
+            return StatusCode(StatusCodes.Status200OK, category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            // return Created();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id < 1) return BadRequest();
+
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (category == null) return NotFound();
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, string name)
+        {
+            if (id < 1) return BadRequest();
+
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (category == null) return NotFound();
+
+            category.Name = name;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
